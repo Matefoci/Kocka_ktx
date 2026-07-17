78e03805702ffcabe9e4b1340b468d7b62153207
Légrády Máté <legrady.mate77@gmail.com>
2026-07-17 13:05:32 +0200

    Fenyek Ki

--- Camera3.js
+++ Camera3.js
@@ -1,6 +1,7 @@
 
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { KTX2Loader } from 'three/addons/loaders/KTX2Loader.js';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xe9e5df);
@@ -32,11 +33,13 @@ const fillLight = new THREE.PointLight(0xffe8cc, 4.2, 14, 2);
 fillLight.position.set(3.2, 2, -2.4);
 fillLight.castShadow = false;
 scene.add(fillLight);
+fillLight.visible = false;
 
 const rimLight = new THREE.PointLight(0x8fb1ff, 1.1, 17, 2);
 rimLight.position.set(-3.2, 2, 2.8);
 rimLight.castShadow = false;
 scene.add(rimLight);
+rimLight.visible = false;
 
 const DEFAULT_FOV = 28;
 const camera = new THREE.PerspectiveCamera(DEFAULT_FOV, window.innerWidth / window.innerHeight, 0.1, 100);
@@ -100,12 +103,17 @@ renderer.shadowMap.type = profile.shadows
 renderer.shadowMap.autoUpdate = false;
 renderer.shadowMap.needsUpdate = true;
 
+
 sun.castShadow = profile.shadows;
 sun.shadow.mapSize.set(profile.shadowMapSize, profile.shadowMapSize);
 sun.shadow.camera.updateProjectionMatrix();
 
 
-
+const ktx2Loader = new KTX2Loader();
+ktx2Loader.setTranscoderPath(import.meta.env.BASE_URL + 'basis/');
+ktx2Loader.detectSupport(renderer);
+const loader = new GLTFLoader();
+loader.setKTX2Loader(ktx2Loader);
 
 // ===== KIEGÉSZÍTŐ FÉNYEK =====
 const fpsOverlay = document.createElement("div");
@@ -201,11 +209,8 @@ function createRotationGizmo(target){
 
 }
 
-    
 
 // Loader
-
-const loader = new GLTFLoader();
 
 let cubeStructure = null;
 let plane = null;
@@ -259,8 +264,8 @@ const LIFT_SPEED = 0.05;
 let baseY = 0;
 
 const modelUrls = [
-    new URL('./models/fKisMeretKocka2.glb', import.meta.url).href,
-    new URL('./models/PlaneMeret.glb', import.meta.url).href,
+    new URL('./models/kocka-ktx2.glb', import.meta.url).href,
+    new URL('./models/plane-ktx2.glb', import.meta.url).href,
     new URL('./models/Nyil6.glb', import.meta.url).href,
 ];
 
